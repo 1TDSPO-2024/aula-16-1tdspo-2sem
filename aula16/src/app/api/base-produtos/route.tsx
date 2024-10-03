a4e25ba811b3cead18387e91ffa1800caae76ba4
fesilva2109 <felipesilvaa.2109@gmail.com>
2024-10-02 23:06:43 -0300

    aula16

--- aula16/src/app/api/base-produtos/route.tsx
+++ aula16/src/app/api/base-produtos/route.tsx
@@ -1,12 +1,29 @@
+import { promises as fs } from "fs";
 import { NextResponse } from "next/server";
-import { promises as fs} from "fs";
 
 export async function GET() {
 
-    const file = await fs.readFile(process.cwd() + "/src/data/base.json" ,"utf-8");
+    const file = await fs.readFile(process.cwd() + "/src/data/base.json", "utf-8");
 
     const dados = JSON.parse(file);
 
     return NextResponse.json(dados);
 }
+export async function POST(request: Request, response: Response) {
 
+    const body = await request.json()
+
+    const file = await fs.readFile(process.cwd() + "/src/data/base.json", "utf-8");
+
+    const dados = JSON.parse(file)
+    const newId = dados[dados.lenght() - 1].id + 1
+    body.id = newId
+    dados.push(body)
+
+    console.log(body)
+
+    await fs.writeFile(process.cwd() + "/src/data/base.json",
+        JSON.stringify({ dados }));
+
+    return NextResponse.json({ body })
+}
