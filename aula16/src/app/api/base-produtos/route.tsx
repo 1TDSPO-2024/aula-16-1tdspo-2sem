0e08f4f8375e2aa268531addb855e17891f3f1c9
FrancescoDiBenedetto <francescomdibe@gmail.com>
2024-10-02 23:03:24 -0300

    inicio do post

--- aula16/src/app/api/base-produtos/route.tsx
+++ aula16/src/app/api/base-produtos/route.tsx
@@ -4,9 +4,19 @@ import { promises as fs} from "fs";
 export async function GET() {
 
     const file = await fs.readFile(process.cwd() + "/src/data/base.json" ,"utf-8");
-
+    
     const dados = JSON.parse(file);
 
     return NextResponse.json(dados);
 }
 
+export async function POST(request: Request) {
+    
+    const file = await fs.readFile(process.cwd() + "/src/data/base.json" ,"utf-8");
+    
+    const {id, nome, preco, marca, cor, modelo, quantidade, categoria, imagem} = await request.json()
+
+
+    return NextResponse.json({msg:"Sucesso"})
+
+}
