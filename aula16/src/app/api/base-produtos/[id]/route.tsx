036ea64fc24f5f8c3748cd885860ae7958a4f415
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-02 23:08:05 -0300

    AAAAAAAA

--- aula16/src/app/api/base-produtos/[id]/route.tsx
+++ aula16/src/app/api/base-produtos/[id]/route.tsx
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { promises as fs} from "fs";
 import { TipoProduto } from "@/types";
 
-export async function GET(request:Request, {params}:{params:{id:number}}) {
+export async function GET(request:Request, {params}:{params:{ id:number }}) {
 
     const file = await fs.readFile(process.cwd() + "/src/data/base.json" ,"utf-8");
 
@@ -11,4 +11,36 @@ export async function GET(request:Request, {params}:{params:{id:number}}) {
     const produto = dados.find( p => p.id == params.id);
 
     return NextResponse.json(produto);
+}
+
+export async function PUT(req: Request, {params}: {params:{ id: string }}) {
+    const body = await req.json();
+
+    if (body.id !== params.id) return NextResponse.json({ msg: "Id não confere" });
+
+    const file = await fs.readFile(process.cwd() + "/src/data/base.json" ,"utf-8");
+
+    const dados = JSON.parse(file);
+
+    type Produto = {
+        id: number,
+        nome: string,
+        preco: number,
+        marca: string,
+        cor: string,
+        modelo: string,
+        quantidade: number,
+        categoria: string,
+        imagem: string
+    }
+
+    const index = dados.findIndex((item: Produto) => item.id === body.id);
+
+    if (index === -1) return NextResponse.json({ msg: "Produto não encontrado"});
+
+    dados[index] = body;
+
+    await fs.writeFile(process.cwd() + "/src/data/base.json", JSON.stringify(dados));
+
+    return NextResponse.json(dados[index]);
 }
--- aula16/src/app/api/base-produtos/route.tsx
+++ aula16/src/app/api/base-produtos/route.tsx
@@ -10,3 +10,17 @@ export async function GET() {
     return NextResponse.json(dados);
 }
 
+export async function POST(req: Request) {
+    const body = await req.json();
+    const file = await fs.readFile(process.cwd() + "/src/data/base.json" ,"utf-8");
+
+    const dados = JSON.parse(file);
+
+    dados.push(body);
+
+    console.log(body);
+
+    await fs.writeFile(process.cwd() + "/src/data/base.json", JSON.stringify(dados));
+
+    return NextResponse.json(dados);
+}
