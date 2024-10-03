c111120dc9d994f58c5f10876fc4521902e3a992
Tcapela <trc2405@gmail.com>
2024-10-02 22:40:50 -0300

    POST

--- aula16/src/app/api/base-produtos/route.tsx
+++ aula16/src/app/api/base-produtos/route.tsx
@@ -10,3 +10,30 @@ export async function GET() {
     return NextResponse.json(dados);
 }
 
+export async function POST(request:Request){
+
+    const file = await fs.readFile(process.cwd() + "/src/data/base.json" ,"utf-8");
+        
+    //const {nome, idade} = await request.json() 
+
+    //console.log("NOME: " + nome);
+    //console.log("IDADE: " + idade);
+
+    //return NextResponse.json({msg:"SUCESSO"});
+
+    const {id, nome, preco, marca, cor, modelo, quantidade, categoria, imagem} = await request.json()
+
+    console.log("ID: " + id);
+    console.log("NOME: " + nome);
+    console.log("PRECO: " + preco);
+    console.log("MARCA: " + marca);
+    console.log("COR: " + cor);
+    console.log("MODELO: " + modelo);
+    console.log("QUANTIDADE: " + quantidade);
+    console.log("CATEGORIA: " + categoria);
+    console.log("IMAGEM: " + imagem);
+
+    return NextResponse.json({msg:"PRODUTO CADASTRADO"});
+
+
+}
