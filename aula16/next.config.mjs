62a6cc2d9ae6e9f8408d1d903b1dc984b2459eda
leonardobarbosas <leonardobarbosantos@gmail.com>
2024-10-02 23:01:23 -0300

    terminando post

--- aula16/next.config.mjs
+++ aula16/next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
-            {
-                protocol: 'https',
-                hostname: 'picsum.photos',
-                port:'',
-                pathname: '/**',
-            }
-        ]
-    }
-};
-
-export default nextConfig;
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "via.placeholder.com",
+          port: "",
+          pathname: "/**",
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
--- aula16/src/app/api/base-produtos/route.tsx
+++ aula16/src/app/api/base-produtos/route.tsx
@@ -1,11 +1,36 @@
 import { NextResponse } from "next/server";
 import { promises as fs } from "fs";
+import { TipoProduto } from "@/types";
 
 export async function GET() {
-  const file = await fs.readFile(process.cwd() + "/src/data/base.json", "utf-8");
+  const file = await fs.readFile(
+    process.cwd() + "/src/data/base.json",
+    "utf-8"
+  );
 
   const dados = JSON.parse(file);
 
   return NextResponse.json(dados);
 }
 
+export async function POST(request: Request) {
+  const file = await fs.readFile(
+    process.cwd() + "/src/data/base.json",
+    "utf-8"
+  );
+
+  const produto = await request.json();
+
+  const dados = JSON.parse(file);
+  const id = dados[dados.length - 1].id + 1;
+  produto.id = id;
+
+  dados.push(produto);
+
+  await fs.writeFile(
+    process.cwd() + "/src/data/base.json",
+    JSON.stringify(dados)
+  );
+
+  return NextResponse.json({ produto });
+}
